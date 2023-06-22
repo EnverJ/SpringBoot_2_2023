1364e0180c66c66966108c4b8ba1badd8f607ec2
EnverJ <Enlem1987@gmail.com>
2023-06-21 19:58:32 -0600

    db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,20 @@
 INSERT INTO employee(id, name, age)
 VALUES('1', 'Eko', 0);
+
+
+/* User Master*/
+ INSERT INTO m_user(
+    user_id,
+    password,
+    user_name,
+    birthday,
+    age,
+    gender,
+    department_id,
+    role
+ )VALUES 
+    ('system@co.jp', 'password', 'System Administrator', '2000-01-01', 23,1,1, 'ROLE_ADMIN'),
+    ('user@co.jp','password','User1', '2000-01-01', 23,2,2, 'ROLE_GENERAL')
+    ;
+
+ 
